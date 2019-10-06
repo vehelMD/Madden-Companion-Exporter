142486a75ecac909d0ac2d902b0ffe963d6c7acc
vehelMD <52833974+vehelMD@users.noreply.github.com>
2019-10-06 05:49:59 +0200

    Test

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const zlib = require('zlib');
 const gzip = zlib.createGzip();
 const fs = require('fs');
-const inp = fs.createReadStream('input.txt');
-const out = fs.createWriteStream('input.txt.gz');
 
 
 
